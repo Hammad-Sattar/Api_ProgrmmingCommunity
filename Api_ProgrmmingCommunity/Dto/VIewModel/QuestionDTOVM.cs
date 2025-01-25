e0f22b005ec712b0750d94a85a91212382bddc31
Hammad sattar <haadiop745@gmail.com>
2025-01-25 22:56:41 +0500

    AddComepetitionRounds

---
+++ Api_ProgrmmingCommunity/Dto/VIewModel/QuestionDTOVM.cs
@@ -0,0 +1,17 @@
+﻿public class QuestionDtoListVM
+    {
+    public int Id { get; set; }
+    public string? SubjectCode { get; set; }
+    public int? TopicId { get; set; }
+    public int? UserId { get; set; }
+    public int? Difficulty { get; set; }
+    public string? Text { get; set; }
+    public int? Type { get; set; }
+    public List<QuestionOptionDTOVM> Options { get; set; }
+    }
+
+public class QuestionOptionDTOVM
+    {
+    public string Option { get; set; }
+    public bool IsCorrect { get; set; }
+    }
