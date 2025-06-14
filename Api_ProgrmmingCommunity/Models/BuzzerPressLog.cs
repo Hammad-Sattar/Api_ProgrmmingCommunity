aa1f45b3e472da70280a5278ad5c9db44f0067ab
Hammad sattar <haadiop745@gmail.com>
2025-06-14 14:30:25 +0500

    buzzerhub+buzzercontroller

---
+++ Api_ProgrmmingCommunity/Models/BuzzerPressLog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Api_ProgrmmingCommunity.Models;
+
+public partial class BuzzerPressLog
+{
+    public int Id { get; set; }
+
+    public int QuestionId { get; set; }
+
+    public int TeamId { get; set; }
+
+    public string? TeamName { get; set; }
+
+    public DateTime PressTime { get; set; }
+
+    public string? PressTimeFormatted { get; set; }
+}
